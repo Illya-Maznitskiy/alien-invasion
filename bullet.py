de0e290771da6de52d04d2b328e4c94b77704a9d
Illya Maznitskiy <comercaleuros@gmail.com>
2024-10-07 16:04:13 +0300

    Add period to documentation bullet.py

--- bullet.py
+++ bullet.py
@@ -18,10 +18,10 @@ class Bullet(Sprite):
         self.y = float(self.rect.y)
 
     def update(self):
-        """Move the bullet up the screen"""
+        """Move the bullet up the screen."""
         self.y -= self.settings.bullet_speed
         self.rect.y = self.y
 
     def draw_bullet(self):
-        """Draw the bullet on the screen"""
+        """Draw the bullet on the screen."""
         pygame.draw.rect(self.screen, self.color, self.rect)
