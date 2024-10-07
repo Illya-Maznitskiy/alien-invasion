957cb7bc5fca7f6973a4e1c5770546b1f4e65790
Illya Maznitskiy <comercaleuros@gmail.com>
2024-10-07 16:12:18 +0300

    Implement documentation in game_stats.py

--- game_stats.py
+++ game_stats.py
@@ -3,8 +3,8 @@ import os
 
 
 class GameStats:
-
     def __init__(self, ai_game):
+        """Initialise the game statistics."""
         self.settings = ai_game.settings
         self.reset_stats()
 
@@ -14,14 +14,13 @@ class GameStats:
 
     @staticmethod
     def get_saved_high_score():
+        """Gets highest score from file."""
         if os.path.exists("high_score.json"):
             with open("high_score.json") as f:
                 try:
-                    # Try loading as a dictionary
                     data = json.load(f)
                     if isinstance(data, dict):
                         return data.get("high_score", 0)
-                    # If it's just an integer
                     elif isinstance(data, int):
                         return data
                 except json.JSONDecodeError:
@@ -37,15 +36,12 @@ class GameStats:
         return 0
 
     def save_high_score(self):
+        """Saves high score to file."""
         with open("high_score.json", "w") as f:
             json.dump({"high_score": self.high_score}, f)
 
     def reset_stats(self):
+        """Initialise the game statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
         self.score = 0
         self.level = 1
-
-    def check_high_score(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
-            self.save_high_score()
