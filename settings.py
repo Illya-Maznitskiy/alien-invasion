e9f05a26a1d2f3559a012894eeaeb65b35515d65
Illya Maznitskiy <comercaleuros@gmail.com>
2024-10-07 16:24:28 +0300

    Implement documentation in settings.py and ship.py

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 class Settings:
     def __init__(self):
+        """Initialize the game's static settings."""
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
@@ -18,18 +19,21 @@ class Settings:
 
         # Bullet settings
         self.bullet_speed = 1.5
-        self.bullet_width = 3
-        self.bullet_height = 15
+        self.bullet_width = 4
+        self.bullet_height = 25
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
+        # How quickly the game speeds up
         self.speedup_scale = 1.1
 
+        # How quickly the alien point values increase
         self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
         self.ship_speed = 1.5
         self.bullet_speed = 3.0
         self.alien_speed = 1.0
@@ -39,6 +43,7 @@ class Settings:
         self.alien_points = 50
 
     def increase_speed(self):
+        """Increase speed settings and alien point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
--- ship.py
+++ ship.py
@@ -4,6 +4,7 @@ from pygame.sprite import Sprite
 
 class Ship(Sprite):
     def __init__(self, ai_game):
+        """Initialize the ship and set its starting position."""
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
@@ -19,6 +20,7 @@ class Ship(Sprite):
         self.moving_left = False
 
     def update(self):
+        """Update the ship's position based on movement indicators."""
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
 
@@ -28,8 +30,10 @@ class Ship(Sprite):
         self.rect.x = self.x
 
     def blitme(self):
+        """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
 
     def center_ship(self):
+        """Center the ship at the center of the screen."""
         self.rect.midbottom = self.screen_rect.midbottom
         self.x = float(self.rect.x)
